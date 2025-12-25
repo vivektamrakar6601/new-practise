b0f20bda5446326ad8fef84af4387cd06511f11b
vivektamrakar6601 <vivektamrakar761@gmail.com>
2025-12-25 13:32:42 +0530

    spash

--- A.java
+++ A.java
@@ -1,3 +1,6 @@
+Pull requests are proposals to merge code changes into a project. A pull request is GitHub's foundational collaboration feature, letting you discuss and review changes before merging them. This helps teams work together, catch issues early, and maintain code quality.
+//////////////////////////////git-stash////////////////////////
+
 public class A {
      public static void main(String[]args){
         int x=100;
